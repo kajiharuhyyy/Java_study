ff98eb3738b7316432adbddc1a39ed280dbf9343
kajiharuhyyy <kajiharu0921@gmail.com>
2025-10-05 20:43:28 +0900

    クラス型と参照

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-        System.err.println("メソッド呼び出すぞい");
-        hello("wii");
-        hello("WEEE");
-        hello("wooo");
-        System.err.println("呼び出したぞい");
-    }
-    public static void hello(String name) {
-        System.err.println(name + "さん");
-    }
+  public static void main(String[] args) {
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
+
+    Matango m2 = new Matango();
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    h.slip();;
+    m1.run();
+    m2.run();
+    h.run();
+    h.attack();
+  }
+
 }
