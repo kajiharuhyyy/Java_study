c5036807aa8abec335b8943a354f2ae9013b8803
kajiharuhyyy <kajiharu0921@gmail.com>
2025-10-06 22:56:31 +0900

    コンストラクタ

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main {
     h.name = "ミナト";
     h.hp = 100;
 
+    Hero h2 = new Hero();
+    h2.name = "アサカ";
+    h2.hp = 100;
+
     Matango m1 = new Matango();
     m1.hp = 50;
     m1.suffix = 'A';
@@ -12,11 +16,27 @@ public class Main {
     m2.hp = 48;
     m2.suffix = 'B';
 
-    h.slip();;
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
+    h.sword = s;
+    System.out.println("現在の武器は" + h.sword.name);
+
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    
+    h.attack();
+    h.slip();
+    w.heal(h);
+    w.heal(h2);
+    w.heal(h2);
     m1.run();
     m2.run();
+
     h.run();
-    h.attack();
+
+
   }
 
 }
